b42ed1b5cc1b2a015f825979896722b2917990b9
lottie Judge <lottie.judge@madetech.com>
2026-06-01 12:10:55 +0100

    adding note for decorator

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+# a decorator = "When an HTTP request matches this URL and method, run the function immediately below."
 @app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
